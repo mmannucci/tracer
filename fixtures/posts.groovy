11549adefe8015132c20b9020c5a34e3ccdb7115
mmannucci <m.mannucci@solvingteam.it>
2011-07-30 16:23:48 +0200

    creata una prima fixture di provcreata una prima fixture di prova

---
+++ fixtures/posts.groovy
@@ -0,0 +1,13 @@
+import org.markomannux.tracer.tokeninput.Post
+fixture {
+    post1(Post, title: "About fixtures") {
+        body = """Fixtures are a way to define data in tests.
+            Data in tests must be predictable and reproducible.
+            There are many frameworks that deals with fixtures."""
+            tags="Fixtures,Testing"
+    }
+    post2(Post, title: "About Grails testing") {
+        body = "Testing in Grails is much easier with fixtures"
+            tags="Fixtures,Testing,Grails"
+    }
+}
