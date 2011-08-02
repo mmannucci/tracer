13af42ce9e8f320816c278988b686025ec592312
mmannucci <m.mannucci@solvingteam.it>
2011-08-02 11:14:00 +0200

    completato test su ordinamento in base a proprietà annidata

--- test/integration/org/markomannux/tracer/blog/PostTests.groovy
+++ test/integration/org/markomannux/tracer/blog/PostTests.groovy
@@ -21,5 +21,14 @@ class PostTests extends GroovyTestCase {
         fixtureLoader['posts'].load('posts_for_ordering')
         def orderedPostList = Post.nestedPropertiesAwareList([sort:'a.name', order:'desc'])
         assertEquals fixtureLoader['posts'].denise, orderedPostList[0].author
+        assertEquals fixtureLoader['posts'].charlie, orderedPostList[1].author
+        assertEquals fixtureLoader['posts'].bob, orderedPostList[2].author
+        assertEquals fixtureLoader['posts'].alice, orderedPostList[3].author
+
+        orderedPostList = Post.nestedPropertiesAwareList([sort:'a.name', order:'asc'])
+        assertEquals fixtureLoader['posts'].alice, orderedPostList[0].author
+        assertEquals fixtureLoader['posts'].bob, orderedPostList[1].author
+        assertEquals fixtureLoader['posts'].charlie, orderedPostList[2].author
+        assertEquals fixtureLoader['posts'].denise, orderedPostList[3].author
     }
 }
